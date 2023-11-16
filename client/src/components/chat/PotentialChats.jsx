a7a61c1291a7892c0925c453d4b199bcae7cfd72
Aryan Gupta <aryangupta02092002@gmail.com>
2023-11-17 00:09:45 +0530

    Socket and Realtime Chat updated

--- client/src/components/chat/PotentialChats.jsx
+++ client/src/components/chat/PotentialChats.jsx
@@ -4,7 +4,7 @@ import { AuthContext } from '../../context/AuthContext';
 
 const PotentialChats = () => {
     const {user} = useContext(AuthContext)
-    const {potentialChats, createChat} = useContext(ChatContext);
+    const {potentialChats, createChat, onlineUsers} = useContext(ChatContext);
 
     return (
         <>
@@ -14,7 +14,7 @@ const PotentialChats = () => {
                         <div className="single-user" key={index}
                          onClick={() => createChat(user._id, u._id)}>
                             {u.name}
-                            <span className="user-online"></span>
+                            <span className={onlineUsers?.some((user) => user?.userId === u?._id) ? "user-online" : ""}></span>
                         </div>
                     );
                 })}
