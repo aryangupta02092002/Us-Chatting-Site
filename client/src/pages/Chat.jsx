aa7a1609b320ddfb15d8e071bb78ca7ceda9b8e2
Aryan Gupta <aryangupta02092002@gmail.com>
2023-11-16 16:08:05 +0530

    Create Chats

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -3,6 +3,7 @@ import { Container, Stack } from "react-bootstrap"
 import { ChatContext } from '../context/ChatContext'
 import UserChat from '../components/chat/UserChat'
 import {AuthContext} from '../context/AuthContext'
+import PotentialChats from '../components/chat/PotentialChats'
 
 const Chat = () => {
   const { user } = useContext(AuthContext);
@@ -13,6 +14,7 @@ const Chat = () => {
 
   return (
     <Container>
+      <PotentialChats/>
       {userChats?.length < 1 ? null : (
         <Stack direction='horizontal' gap={4} className='align-items-start'>
           <Stack className='messages-box flex-grow-0 pe-3' gap={3}>
